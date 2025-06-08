5492ffe7a7b32e78c68643e4dbf1dc36c2ff6c41
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-06-07 20:30:10 -0400

    feat: modelo de team agregado

---
+++ app/Models/Team.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Multitenancy\Models\Tenant;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Team extends Tenant
+{
+    use HasSlug;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate()
+            ->slugsShouldBeNoLongerThan(50);
+    }
+
+    public function getTenantKey()
+    {
+        return $this->getKey();
+    }
+    public function getDatabaseName(): string
+    {
+        // Retorna el nombre de tu base de datos central
+        return env('DB_DATABASE', 'laravel');
+    }
+
+    // public function getDatabaseName(): string
+    // {
+    //     return $this->database_name ?? $this->generateDatabaseName();
+    // }
+
+    // protected function generateDatabaseName(): string
+    // {
+    //     $prefix = config('multitenancy.database.prefix', 'tenant_');
+    //     $this->database_name = $prefix . $this->slug;
+    //     $this->save();
+
+    //     return $this->database_name;
+    // }
+}
